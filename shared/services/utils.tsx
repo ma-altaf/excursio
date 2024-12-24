05a242ef6cbd41cbe1ec3501235bd219f367a31b
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2024-12-23 19:32:56 -0800

    Date DateMap converter functions

--- shared/services/utils.tsx
+++ shared/services/utils.tsx
@@ -1,23 +1,27 @@
-export const today = new Date();
+export type DateMap = Map<number, Map<number, Map<number, boolean[]>>>;
+
+export const today = resetTime(new Date());
 
 export const fullDay = new Array<boolean>(24).fill(true);
 
-export const formatDate = (date: Date) =>
-  new Intl.DateTimeFormat("en-CA", {
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat("en-CA", {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
   }).format(date);
+}
 
-export const formatTime = (date: Date) =>
-  new Intl.DateTimeFormat("en-CA", {
+export function formatTime(date: Date) {
+  return new Intl.DateTimeFormat("en-CA", {
     timeStyle: "long",
   }).format(date);
+}
 
-export const resetTime = (date: Date) => {
+export function resetTime(date: Date) {
   date.setHours(0, 0, 0, 0);
   return date;
-};
+}
 
 export const namedDays = [
   "Sunday",
@@ -43,3 +47,47 @@ export const namedMonths = [
   "November",
   "December",
 ];
+
+export function datesToDateMap(dates: Map<string, boolean[]>) {
+  const res: DateMap = new Map(new Map(new Map()));
+
+  dates.forEach((value, key) => {
+    const d = key.split("-");
+
+    const year = Number(d[0]);
+    const month = Number(d[1]) - 1;
+    const date = Number(d[2]);
+
+    if (!res.get(year)) {
+      res.set(year, new Map());
+    }
+
+    if (!res.get(year)?.get(month)) {
+      res.get(year)?.set(month, new Map());
+    }
+
+    res.get(year)?.get(month)?.set(date, value);
+  });
+
+  return res;
+}
+
+export function dateMapToDates(dateMap: DateMap) {
+  const res = new Map();
+
+  dateMap.keys().forEach((year) => {
+    dateMap
+      .get(year)
+      ?.keys()
+      .forEach((month) => {
+        dateMap
+          .get(year)
+          ?.get(month)
+          ?.forEach((value, date) => {
+            res.set(`${year}-${month}-${date}`, value);
+          });
+      });
+  });
+
+  return res;
+}
