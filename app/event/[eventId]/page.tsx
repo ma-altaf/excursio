f5a824bb76a1a96de57dadd81a679463c8dbf85a
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2025-01-01 19:35:23 -0800

    Update page.tsx

--- app/event/[eventId]/page.tsx
+++ app/event/[eventId]/page.tsx
@@ -26,7 +26,7 @@ export default async function Page({
   const { ownerId, title, description } = eventData;
 
   return (
-    <section className="w-full min-h-screen flex flex-col md:px-[10%] lg:px-[20%]">
+    <section className="w-full min-h-screen flex flex-col p-2 md:px-[10%] lg:px-[20%]">
       <div className="flex flex-col justify-center items-center w-full mb-4">
         <h1 className="text-3xl p-4">{title}</h1>
         <EditBtn ownerId={ownerId} eventId={eventId} />
