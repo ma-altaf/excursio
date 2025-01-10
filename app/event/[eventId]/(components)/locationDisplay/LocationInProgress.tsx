ce10e3039ee38b27ef07f812185d62177bf6fedf
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2025-01-10 13:02:07 -0800

    Update LocationInProgress.tsx

--- app/event/[eventId]/(components)/locationDisplay/LocationInProgress.tsx
+++ app/event/[eventId]/(components)/locationDisplay/LocationInProgress.tsx
@@ -13,7 +13,7 @@ export default function LocationInProgress({
     <span className="w-full flex flex-col">
       <span>
         {suggestions ? (
-          <ul className="flex flex-col w-full mt-2 border-2 border-black rounded-md px-2 py-1">
+          <ul className="flex flex-col w-full mt-1 border-2 border-black rounded-md px-2 py-1">
             <p>Your suggestions:</p>
             {suggestions.map((el, i) => (
               <SelectedLocationItems key={i} location={el} />
@@ -24,12 +24,18 @@ export default function LocationInProgress({
         )}
       </span>
 
-      <Link
-        href={`/event/${eventId}/participate/location`}
-        className="mt-1 p-button rounded-md bg-accent w-fit"
-      >
-        Participate
-      </Link>
+      {!suggestions ? (
+        <Link
+          href={`/event/${eventId}/participate/location`}
+          className="mt-1 p-button rounded-md bg-accent w-fit"
+        >
+          Participate
+        </Link>
+      ) : (
+        <p className="mt-1 p-1 rounded-md bg-gray-100 border-2 border-gray-200 text-center">
+          Wait for organizer to move to voting stage.
+        </p>
+      )}
     </span>
   );
 }
