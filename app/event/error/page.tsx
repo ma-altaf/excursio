20c71e837ec67e066b3fba416520facd8deea6d3
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2025-02-04 17:38:17 -0800

    Update event error page

--- app/event/error/page.tsx
+++ app/event/error/page.tsx
@@ -1,7 +1,8 @@
 export default function NoEvent() {
   return (
-    <section className="w-full min-h-screen flex justify-center items-center">
-      <h1 className="text-2xl">Could not found event.</h1>
+    <section className="w-full min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-4">Could not find Event</h1>
+      <p>Check if the event link is correct.</p>
     </section>
   );
 }
