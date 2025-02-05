f3d51a6cd339cdbec69a05f4d720b474622c1e8a
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2025-02-04 17:27:14 -0800

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <main className="w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="text-9xl font-bold">404</h1>
+      <p className="my-4">Page Not Found!</p>
+      <Link href="/" className="p-button rounded-full bg-accent">
+        Go To Homepage
+      </Link>
+    </main>
+  );
+}
