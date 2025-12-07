628df78d119de0c82b22caff72aabae4045f98d2
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2025-12-07 08:53:57 +0400

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,23 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  distDir: "build",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-      },
-    ],
-  },
+    distDir: "build",
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "127.0.0.1",
+            },
+            {
+                protocol: "https",
+                hostname: "firebasestorage.googleapis.com",
+            },
+        ],
+    },
+    turbopack: {
+        root: path.join(__dirname),
+    },
 };
 
 export default nextConfig;
